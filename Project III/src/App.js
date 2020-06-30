a29f32ef276233f2b10986f9e4ade4b907f95f0f
AlbertoGiffard <albertoggm1@gmail.com>
2020-06-29 21:17:37 -0300

    forms

--- Project III/src/App.js
+++ Project III/src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import './App.css'
+import React, {Component, forwardRef} from 'react';
+/* import './App.css'
+import Chart from 'chart.js'; */
 /* Iteraciones */
 /* const frutas = [
   'fresa',
@@ -291,7 +292,7 @@ class App extends Component {
 } */
 
 /* pasar datos al iterar listas con react */
-class App extends Component{
+/* class App extends Component{
   state = {
     fruits: [
       { name: 'Fresa', price: 5.4 },
@@ -329,6 +330,394 @@ class App extends Component{
           </li>
         ))}
       </ul>
+    )
+  }
+} */
+
+/* Referencias o refs con React */
+/* class Entrada extends Component{
+  constructor() {
+    super()
+
+    //aca dentro del constructor tenemos que llamar al metodo de react createRef
+    this.entrada = React.createRef()
+  }
+  //la siguiente funcion es parte del ciclo de vida de una funcion, esta funcion se ejecuta cuando el componente es montado
+  componentDidMount(){
+
+  }
+
+  focus = () =>{
+    //aca con la siguiente linea hace foco al campo input
+    this.entrada.current.focus()
+    
+  }
+
+  blur = () =>{
+    //en la siguiente linea elimina el foco del elemento referenciado
+    this.entrada.current.blur()
+  }
+  render() {
+    return(
+      <div>
+        {/* //le colocamos ref como propiedad al tag que queremos hacer referencia 
+        <input type="text" ref={this.entrada}/>
+        <button onClick={this.focus}>
+          Focus
+        </button>
+        <button onClick={this.blur}>
+          Blur
+        </button>
+      </div>
+    )
+  }
+}
+class App extends Component{
+  render() {
+    return(
+      <div>
+        <h1>
+          React refs
+        </h1>
+        <Entrada/>
+      </div>
+    )
+  }
+}
+ */
+
+/* Integrar librerias de terceros usando las refs de React */
+
+// las graficas se sacaron de chart.js se instalan con npm y luego la importo al principio 
+/* class Graficas extends Component{
+  // hago referencia al ref
+  grafica = React.createRef();
+  componentDidMount() {
+    //cambio var por const ya que son constantes porque nunca van a variar
+    //elimino el getelementbyid por this.grafica.current para acceder al contenido del tag canvas
+    const ctx = this.grafica.current.getContext('2d');
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+  }
+  render() {
+    return(
+      <div>
+        <canvas
+        //hago la refencia a este tag
+          ref={this.grafica}
+          width="400"
+          height="400"
+        >
+
+        </canvas>
+      </div>
+    )
+  }
+}
+
+class App extends Component{
+  render() {
+    return(
+      <div>
+        <h1>Usando refs</h1>
+        <Graficas/>
+      </div>
+    )
+  }
+} */
+
+/* reenvio de la prop especial ref con forwardRef */
+//sirve para reenviar la ref del padre al hijo
+/* const FancyInput = React.forwardRef((props, ref) =>(
+  <div>
+    <input type="text" ref={ref}/>
+  </div>
+))
+class App extends Component{
+  entrada = React.createRef()
+
+  componentDidMount(){
+    console.log(this.entrada);
+    
+  }
+  render() {
+    return(
+      <div>
+        <h1>Reenvio de refs</h1>
+        <FancyInput ref={this.entrada}/>
+      </div>
+    )
+  }
+}  */
+
+/* manejando inputs no controlados con refs */
+
+/* const Unicorn = () =>{
+  return(
+    <span>
+      Imagen
+    </span>
+  )
+}
+
+class InputNoControlado extends Component{
+
+
+  handleSubmit = (e) =>{
+    //aca obtengo los valores del ref de la siguiente forma
+    // no olvidar el current.value para obtener el valor
+
+    //respecto a los inputs controlados, cuando le pase el evento(e) como paremetro al ser un form, le puedo pasar .target y el valor que pase dentro de los [] indica cual de los input va a tomar, es decir, si paso e.target[0] este me tomaria el primer input del form
+    //paso preventdefault para que no recargue la pagina y se puedan quedar los cambios guardados
+    e.preventDefault()
+    const nombre = e.target[0].value
+    const email = e.target[1].value
+    //manejo de datos
+    this.props.onSend({nombre, email})
+  }
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder='Nombre'/>
+        <input type="text" placeholder='Email'/>
+        <button>
+          Enviar
+        </button>
+      </form>
+    )
+  }
+}
+class App extends Component{
+  send = (data) =>{
+    console.log(data);
+    
+  }
+  render() {
+    return(
+      <div>
+        <h1>
+          Inputs no controlados refs <Unicorn/>
+        </h1>
+        <InputNoControlado
+          onSend={this.send}
+        />
+      </div>
+    )
+  }
+} */
+
+/* Ventajas y desventajas de inputs controlados y no controlados */
+/* en los inputs no controlados su sintaxis se parece mas a html puro, el cual lo hace mas visible de visualizar, tiene en desventaja que la informacion que suministre el usuario solo podra ser revisada cuando el usuario la envie, antes de esto no se podra corregir o modificar */
+
+/* Inputs controlados */
+/* class InputControlado extends Component{
+  state = {
+    text: '',
+    tieneError: false,
+    color: '#E8E8E8'
+  }
+
+  actualizar = (e) =>{
+    const text= e.target.value
+    const tieneError = text !== '' && text.length < 5
+    //le coloco let ya que este si va ha variar
+    let color = 'green'
+    //trim limpia los espacios en blanco al principio y al final
+    if (text.trim() === ''){
+      color= '#E8E8E8'
+    }
+
+    if (text.trim() !== '' && text.trim().length < 5){
+      color= 'red'
+    }
+
+    this.setState({ text, color })
+    
+    //propagando datos al padre
+    this.props.onChange(this.props.name, text)
+  }
+
+  render() {
+    const styles = {
+      border: `1px solid ${this.state.color}`,
+      padding: '0.3em 0.6em',
+      outline: 'none'
+    }
+    return(
+      <input 
+        type="text"
+        value={this.state.text}
+        onChange={this.actualizar}
+        style={styles}
+        placeholder={this.props.placeholder}
+      />
+    )
+  }
+}
+class App extends Component{
+  state = {
+    name: '',
+    email: ''
+  }
+  actualizar = (name, text) =>{
+    this.setState({
+      [name]: text
+    })
+    
+  }
+  render() {
+    return(
+      <div>
+        <h1>Inputs Controlados</h1>
+        <InputControlado
+          onChange={this.actualizar}
+          placeholder='Nombre Completo'
+          //le paso este valor para que sea reutilizable el codigo y asi no tener que repetir codigo para recibir los distintos parametros
+          name='name'
+        />
+        <InputControlado
+          onChange={this.actualizar}
+          placeholder='Tu Email'
+          name='email'
+        />
+        <h2>
+          Nombre: {this.state.name}
+        </h2>
+        <h2>
+          Email: {this.state.email}
+        </h2>
+      </div>
+    )
+  }
+} */
+
+/* usando el atributo for en formularios */
+/* class InputNoControlado extends Component{
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label htmlFor='name'>Nombre:</label>
+          <input 
+            type="text"
+            placeholder='Nombre'
+            id='name'
+          />
+        </p>
+        <p>
+          <label htmlFor='email'>E-mail:</label>
+          <input 
+            type="text"
+            placeholder='Email'
+            id='email'
+          />
+        </p>
+        <button>
+          Enviar
+        </button>
+      </form>
+    )
+  }
+}
+
+
+class App extends Component{
+
+  send = (data) =>{
+    console.log(data);
+    
+  }
+  render() {
+    return(
+      <div>
+        <h1>
+          Atributo ref
+        </h1>
+        <InputNoControlado
+          onSend={this.send}
+        />
+      </div>
+    )
+  }
+} */
+
+/* formulario con ejemplo de seleccion */
+class App extends Component {
+  state = {
+    techs: ['Vue']
+  }
+
+  handleChange = (e) =>{
+    //al ser objetos de html no me los devuelve como un array por lo tanto al pasarlo como array.from lo vuelve un array legible, luego lo mapeo para poder visualizar los objetos del array, dentro del array.from como segundo parametro se pasa el map
+    const techs = Array.from(
+      e.target.selectedOptions,
+      ( option ) =>( option.value )
+    )
+    console.log(techs);
+    this.setState({
+      techs
+    })
+  }
+  render () {
+    return(
+      <div>
+        <h1>
+          Etiqueta Select 
+        </h1>
+        <form>
+          {/* la propiedad dentro del tag selected hace que por defecto este seleccionado el valor que coloquemos dentro sin la necesidad de pasarle selected a la opcion que necesitamos que se muestre de primero */}
+          <select 
+            onChange={this.handleChange}
+            multiple
+          >
+            <option value="Angular">Angular</option>
+            <option value="React">React</option>
+            <option value="Vue">Vue</option>
+            <option value="Vanilla">Vanilla</option>
+          </select>
+        </form>
+        <ul>
+          {this.state.techs.map((tech) =>(
+            <li key={tech}>
+              { tech }
+            </li>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
---
+++ Project IV/src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+const Gato = (props) =>{
+  return( 
+    <div>
+      <h1>Gato</h1>
+      <pre>
+        {JSON.stringify(props, null, 4)}
+      </pre>
+    </div>
+  )
+}
+
+class App extends Component {
+  state = {
+    fuerza: 100,
+    vidasRestantes: 7
+  }
+
+  render () {
+    const otrosDatos = {
+      raza: 'tropical',
+      peleasNocturnas: 300
+    }
+    return (
+      <div>
+        <Gato 
+          name='Garfield' 
+          age='2 años'
+          { ...otrosDatos }  
+          { ...this.state }
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
